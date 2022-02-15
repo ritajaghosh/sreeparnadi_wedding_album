6be872f66b3f2c5d5315e1f934a6de2f2a823ac0
sukantahui <huisukanta77@gmail.com>
2022-02-16 01:06:34 +0530

    light gallery added with more picture2

--- src/app/home/hui-light-gallery/hui-light-gallery.component.ts
+++ src/app/home/hui-light-gallery/hui-light-gallery.component.ts
@@ -18,10 +18,11 @@ export class HuiLightGalleryComponent implements OnInit {
     {
       id: '1',
       size: '1400-933',
+      gdrive:"https://drive.google.com/uc?export=view&id=",
       src:
-        'https://drive.google.com/uc?export=view&id=1X-9Mrb7xcytyDlPLE6fq8VwmYppLBjw7',
+        '1X-9Mrb7xcytyDlPLE6fq8VwmYppLBjw7',
       thumb:
-        'https://drive.google.com/uc?export=view&id=1oncl1HDhG05riV31couX_0ZdQ35v_l5S',
+        '1oncl1HDhG05riV31couX_0ZdQ35v_l5S',
       subHtml: `<div class="lightGallery-captions">
             <h4>Photo by <a href="https://unsplash.com/@dann">Dan</a></h4>
             <p>Published on November 13, 2018</p>
@@ -30,10 +31,11 @@ export class HuiLightGalleryComponent implements OnInit {
     {
       id: '2',
       size: '1400-933',
+      gdrive:"https://drive.google.com/uc?export=view&id=",
       src:
-        'https://drive.google.com/uc?export=view&id=11LS6uO6eQvORIOu5YVBqjx-Rvuhk4yiq',
+        '11LS6uO6eQvORIOu5YVBqjx-Rvuhk4yiq',
       thumb:
-        'https://drive.google.com/uc?export=view&id=1MfgYmAmEgRxgke-oR-zGYY2b4xlFc_gj',
+        '1MfgYmAmEgRxgke-oR-zGYY2b4xlFc_gj',
       subHtml: `<div class="lightGallery-captions">
             <h4>Photo by <a href="https://unsplash.com/@kylepyt">Kyle Peyton</a></h4>
             <p>Published on September 14, 2016</p>
@@ -42,10 +44,11 @@ export class HuiLightGalleryComponent implements OnInit {
     {
       id: '3',
       size: '1400-932',
+      gdrive:"https://drive.google.com/uc?export=view&id=",
       src:
-        'https://drive.google.com/uc?export=view&id=1qcIxB32-I06VMbRhCTQUlnkstgH9KXV5',
+        '1qcIxB32-I06VMbRhCTQUlnkstgH9KXV5',
       thumb:
-        'https://drive.google.com/uc?export=view&id=1ghSvXIJp3GAr6YHdQt2kp0Go7OFyyLiL',
+        '1ghSvXIJp3GAr6YHdQt2kp0Go7OFyyLiL',
       subHtml: `<div class="lightGallery-captions">
             <h4>Photo by <a href="https://unsplash.com/@jxnsartstudio">Garrett Jackson</a></h4>
             <p>Published on May 8, 2020</p>
@@ -54,10 +57,11 @@ export class HuiLightGalleryComponent implements OnInit {
     {
       id: '4',
       size: '1400-932',
+      gdrive:"https://drive.google.com/uc?export=view&id=",
       src:
-        'https://drive.google.com/uc?export=view&id=1SZsBbcGUn8KoMRkE76LKNTwPQLb5MRK_',
+        '1SZsBbcGUn8KoMRkE76LKNTwPQLb5MRK_',
       thumb:
-        'https://drive.google.com/uc?export=view&id=1qMPVzPHdpJSkhtFU3xjesnMTZ5PJc3nr',
+        '1qMPVzPHdpJSkhtFU3xjesnMTZ5PJc3nr',
       subHtml: `<div class="lightGallery-captions">
             <h4>Photo by <a href="https://unsplash.com/@jxnsartstudio">Garrett Jackson</a></h4>
             <p>Published on May 8, 2020</p>
@@ -76,23 +80,23 @@ export class HuiLightGalleryComponent implements OnInit {
   onInit = (detail: { instance: LightGallery; }): void => {
     this.lightGallery = detail.instance;
   };
-  addImage = () => {
-    this.items = [
-      ...this.items,
-      {
-        id: '4',
-        size: '1400-933',
-        src:
-          'https://images.unsplash.com/photo-1609902726285-00668009f004?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1400&q=80',
-        thumb:
-          'https://images.unsplash.com/photo-1609902726285-00668009f004?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=240&q=80',
-        subHtml: `<div class="lightGallery-captions">
-            <h4>Photo by <a href="https://unsplash.com/@bruno_adam">Bruno Adam</a></h4>
-            <p>Published on January 6, 2021</p>
-        </div>`
-      }
-    ];
-    this.needRefresh = true;
-  };
+  // addImage = () => {
+  //   this.items = [
+  //     ...this.items,
+  //     {
+  //       id: '4',
+  //       size: '1400-933',
+  //       src:
+  //         'https://images.unsplash.com/photo-1609902726285-00668009f004?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1400&q=80',
+  //       thumb:
+  //         'https://images.unsplash.com/photo-1609902726285-00668009f004?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=240&q=80',
+  //       subHtml: `<div class="lightGallery-captions">
+  //           <h4>Photo by <a href="https://unsplash.com/@bruno_adam">Bruno Adam</a></h4>
+  //           <p>Published on January 6, 2021</p>
+  //       </div>`
+  //     }
+  //   ];
+  //   this.needRefresh = true;
+  // };
 
 }
